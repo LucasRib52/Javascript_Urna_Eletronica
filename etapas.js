262f17e7e3537997c684a49b1f9264145b0af2c4
Lucas <lucasribeiroprogramador@gmail.com>
2022-03-04 05:56:02 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '69696',
+                nome: 'Patrick do sax',
+                partido: 'Free fire',
+                fotos:[
+                    {url:'69696.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '71717',
+                nome: 'Lucas',
+                partido: 'FIFA',
+                fotos:[
+                    {url:'71717.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '71',
+                nome: 'Marcos Bolsonaro',
+                partido: 'cerveja',
+                vice: 'fabio',
+                fotos:[
+                    {url:'84.jpeg', legenda: 'Prefeito'},
+                    {url:'84_2.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
